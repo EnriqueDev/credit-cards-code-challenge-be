f28f0f006280e26eba525dba6ef3d0b782fd40e6
Enrique Martín García <enrique@emartin.dev>
2020-01-30 00:57:36 +0100

    added cors middleware

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,16 @@
 import App from './app'
 import CardsControler from './modules/Cards/CardsControler'
 import bodyParser = require('body-parser')
+import cors from 'cors'
 
 const app = new App({
-  port: 1234,
+  port: 4321,
   controllers: [new CardsControler()],
-  middleware: [bodyParser.json(), bodyParser.urlencoded({ extended: true })],
+  middleware: [
+    bodyParser.json(),
+    bodyParser.urlencoded({ extended: true }),
+    // Note: a generic cors was used here, in a real-world app should be client-restricted
+    cors(),
+  ],
 })
 app.listen()
